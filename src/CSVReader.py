5693b8431d80c29a4d3ab67a19e80edab428f3aa
Faraaz <madnifaraaz@gmail.com>
2023-01-31 21:12:16 +0300

    Added website filter. Added a To Do List. Beautified the code a little.

--- src/CSVReader.py
+++ src/CSVReader.py
@@ -1,7 +1,11 @@
+
+
 """
-File 1 of 3.
+Functions 1 of 3 implemented.
 Responsible for: Parsing CSV and extracting Domain Name from the lead's email.
 """
+
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -25,7 +29,7 @@ def extract_website(sourcefile):
             for row in csvreader:        
                 i += 1 
                 print(i, " \n")
-                # if i < 10 and i > 2: # In case you need to specify select rows.
+                # if i < 10 and i > 2: # In case you need to specify select rows.   #+
                 # Assigning the values from the fields in the CSV to variables. You may replace the field names to suit your CSV.
                 fac_name, fac_email = row['Facility Name'], row['Facility Email'] 
 
@@ -33,7 +37,8 @@ def extract_website(sourcefile):
                 print ("Facility E-mail is ", fac_email)
 
                 fac_website = fac_email[fac_email.index('@') + 1 :]
-                if (fac_website != "GMAIL.COM" and fac_website != "YAHOO.COM"):
+                email_providers_list = ["GMAIL.COM","YAHOO.COM","AOL.COM","HOTMAIL.COM","PROTONMAIL.COM"] #Filter out email providers, leaving just the website URLS of the agencies.
+                if (fac_website not in email_providers_list):
                     # Successfully extracted the Domain Website so this can be automated.
                     print ("Extracted Website: ", fac_website)
                     automated += 1 
@@ -45,7 +50,7 @@ def extract_website(sourcefile):
                     manual += 1 
                     manual_list.append(fac_name)
                 # else: 
-                #     print ("Row Limit Reached.")
+                #     print ("Row Limit Reached.")                                  #+
 
                 print("-----------")
             
@@ -67,25 +72,33 @@ def extract_website(sourcefile):
 def crawl_website(automated_list):
     print ("Websites Extracted: ", automated_list)
     for j in automated_list:
+        j = "HTTPS://WWW." + j
         print ("Currently looking at: ", j) #COME BACK TO THIS LATER, TESTING WITH A SINGLE WEBSITE RN.
         webpage = urlopen("HTTPS://WWW.NURSENEXTDOOR.COM")
         extracted_html = webpage.read().decode("utf-8")
 
-        extracted_soup = BeautifulSoup(extracted_html,"html.parser")
-        extracted_soup.get_text()
-        print(extracted_soup)
-
+        soup = BeautifulSoup(extracted_html,"html.parser")
+        soup.get_text()
+        soup_filter = re.findall(r'\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}', soup.text)
+        print(soup_filter)
+        
         break
 
+        
 extract_website('src/PythonAutomationSource.csv')
 
+"""                                                     TO DO
 
-# Using the Pandas module.
-# import pandas as pd
+1.  Function to check extracted website's robots.txt to see if website can be scraped.
+2.  Check the website's terms of service to see whether scraping the website has been explicitly prohibited.
+3.  Check if the website offers an API.                                                                       - no scraping required then, but highly unlikely.
+4.  Use a rotating Residential IP.                                                                            - costly!
+5.  Crawl to find page that has a specified branch's detail.
+6.  Extract address of the branch.
+7.  Extract contact number.
+8.  Extract company name. 
+9.  Store extracted website data in a CSV.
+10. Upload data row by row into Hubspot.
 
-# fac_name    = pd.read_csv('src/PythonAutomationSource.csv', usecols=['Facility Name'])
-# fac_email   = pd.read_csv('src/PythonAutomationSource.csv', usecols=['Facility Email'])
-
-# for i in fac_name:
-#     print(i)
+"""
         
